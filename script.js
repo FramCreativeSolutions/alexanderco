35af46e35548b5fde3f9beeda7f3e9f52c8ced82
Elizabet Angelova <elizabet@framcreativesolutions.com>
2021-06-21 12:36:55 +0300

    Finishing map functionality

--- script.js
+++ script.js
@@ -124,12 +124,141 @@ window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
 
-let usa = document.getElementsByClassName('usa')[0];
+//---------------------------------------------------------------------------------------------------------------------
 let products = document.getElementsByClassName("products")[0]
+let scrolltoObj = document.getElementsByClassName('sri-lanka')[0]
+
+let usa = document.getElementsByClassName('usa')[0];
 usa.addEventListener('click',() => {
-document.getElementsByClassName("products")[0].innerHTML = `<img src="./3.png" class="flag"/><strong>Products from the USA</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Cereal</div><div>Chips</div><div>Popcorn</div><div>Oils</div>`
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./3.png" class="flag flag2"/><strong>Products from the USA</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Cereal</div><div>Chips</div><div>Popcorn</div><div>Oils</div>`
 products.style.display = 'block'
-products.scrollIntoView({ 
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let hungary = document.getElementsByClassName('hungary')[0];
+hungary.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./5.png" class="flag flag2"/><strong>Products from Hungary</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Bourbon</div><div>Pet food</div><div>Snack foods</div><div>Meat</div><div>Vinegar</div><div>Oil seeds</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let italy = document.getElementsByClassName('italy')[0];
+italy.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./10.png" class="flag flag2"/><strong>Products from Italy</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Wines</div><div>Olives</div><div>Pasta</div><div>Flour</div><div>Pesto</div><div>Rice</div><div>Processed vegetavles</div><div>Fish</div><div>Coffee</div><div>Oils</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let spain = document.getElementsByClassName('spain')[0];
+spain.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./11.png" class="flag flag2"/><strong>Products from Spain</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Dry fruits and nuts</div><div>Wines</div><div>Citrus fruits</div><div>Pork meat</div><div>Saffron</div><div>Jam</div><div>Olives and olive oil</div><div>Honey</div><div>Jamon (ham)</div><div>Oils</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let mexico = document.getElementsByClassName('mexico')[0];
+mexico.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./4.png" class="flag flag2"/><strong>Products from Mexico</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Avocado oil</div><div>Pecan nuts</div><div>Spirits</div><div>Vinegar</div><div>Soybeans</div><div>Corn</div><div>Avocados</div><div>Lime & berry fruits</div><div>Sugar & sweeteners</div><div>Peppers & chile</div><div>Dairy products</div><div>Tomatoes</div><div>Tequila & mezcal</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let chile = document.getElementsByClassName('chile')[0];
+chile.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./2.png" class="flag flag2"/><strong>Products from Chile</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Fish fillets</div><div>Wine</div><div>Nuts</div><div>Corn</div><div>Fresh fruit</div><div>Dairy</div><div>Meat</div><div>Vegetables</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let japan = document.getElementsByClassName('japan')[0];
+japan.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./13.png" class="flag flag2"/><strong>Products from Japan</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Noodles</div><div>Seaweed</div><div>Matcha Tea</div><div>Soy Sauce</div><div>Fresh fruit</div><div>Tofu</div><div>Fish</div><div>Miso Soup</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let sri_lanka = document.getElementsByClassName('sri-lanka')[0];
+sri_lanka.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./7.png" class="flag flag2"/><strong>Products from Sri Lanka</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Cococnut</div><div>Arak</div><div>Nuts</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let hong_kong = document.getElementsByClassName('hong-kong')[0];
+hong_kong.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./9.png" class="flag flag2"/><strong>Products from Hong Kong</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Seaweed</div><div>Rice</div><div>Nuts</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let singapore = document.getElementsByClassName('singapore')[0];
+singapore.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./8.png" class="flag flag2"/><strong>Products from Singapore</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Dry fruits</div><div>Wine</div><div>Non-alcoholic beverages</div><div>Nuts</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let thailand = document.getElementsByClassName('thailand')[0];
+thailand.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./14.png" class="flag flag2"/><strong>Products from Thailand</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Rice</div><div>Canned tuna</div><div>Sugar</div><div>Cassava meat</div><div>Canned Pineapple</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let new_zealand = document.getElementsByClassName('new-zeeland')[0];
+new_zealand.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./6.png" class="flag flag2"/><strong>Products from New Zeeland</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Wine</div><div>Cotton</div><div>Sugar</div><div>Oil</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let australia = document.getElementsByClassName('australia')[0];
+australia.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./12.png" class="flag flag2"/><strong>Products from Australia</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Cereal</div><div>Honey</div><div>Dairy</div><div>Meat</div><div>Dry fruits</div><div>Vinegar</div><div>Nuts</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let canada = document.getElementsByClassName('canada')[0];
+canada.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./15.png" class="flag flag2"/><strong>Products from Canada</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Maple syrup</div><div>Bacon</div><div>Coffee</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
+  behavior: 'smooth' 
+});
+})
+
+let greece = document.getElementsByClassName('greece')[0];
+greece.addEventListener('click',() => {
+document.getElementsByClassName("products")[0].innerHTML = `<img src="./1.png" class="flag flag2"/><strong>Products from Greece</strong><hr class="products-hr"/><div style="height: 10px;"></div><div>Bottarga</div><div>Feta cheese</div><div>Olive oil</div><div>Pomegranate juice</div>`
+products.style.display = 'block'
+scrolltoObj.scrollIntoView({ 
   behavior: 'smooth' 
 });
 })
